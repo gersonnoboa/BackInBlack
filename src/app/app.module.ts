cdc1cf0f14b61acccb349dcb793f968b2e149199
Gerson Noboa <gerson.noboa@gmail.com>
2018-02-09 16:55:02 +0200

    Routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialModule } from './material/material.module';
+import { Task1Component } from './task1/task1.component';
+import { Task2Component } from './task2/task2.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  { path: 'task1', component: Task1Component },
+  { path: 'task2', component: Task2Component },
+  { path: '', redirectTo: 'task1', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    Task1Component,
+    Task2Component,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(appRoutes),
   ],
   exports: [
     MaterialModule
